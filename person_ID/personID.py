dd88ee9a1f482a7f2640f7451a025ae3dfbaaf09
devashish bhake <devrajbhake@gmail.com>
2023-10-16 20:01:03 +0530

    modified:   person_ID/personID.py

--- person_ID/personID.py
+++ person_ID/personID.py
@@ -6,14 +6,14 @@ warnings.filterwarnings("ignore")
 
 
 # video inputs
-video_1 = "/home/devashish/Desktop/work/Person_ReID/person_ID/test_1.mp4"
-video_2 = "/home/devashish/Desktop/work/Person_ReID/person_ID/test_2.mp4"
-video_3 = "/home/devashish/Desktop/work/Person_ReID/person_ID/test_3.mp4"
-video_4 = "/home/devashish/Desktop/work/Person_ReID/person_ID/test_4.mp4"
+video_1 = "/home/devashish/Desktop/work/Person_reID/person_ID/test_1.mp4"
+video_2 = "/home/devashish/Desktop/work/Person_reID/person_ID/test_2.mp4"
+video_3 = "/home/devashish/Desktop/work/Person_reID/person_ID/test_3.mp4"
+video_4 = "/home/devashish/Desktop/work/Person_reID/person_ID/test_4.mp4"
 # change the above paths to your vwebcam ids if multiple webcams are connected
 #example: video_1 = 0, video_2 = 1, video_3 = 2 and so on where 0, 1, 2 ... are the webcam ids of the webcams connected either default or via usb connectors
 
-model = YOLO("/home/devashish/Desktop/work/Person_ReID/person_ID/personID.pt")
+model = YOLO("/home/devashish/Desktop/work/Person_reID/person_ID/personID.pt")
 print('model loaded')
 
 def run_tracker_in_thread(filename, model, file_index):
@@ -43,8 +43,16 @@ tracker_thread1 = multiprocessing.Process(target=run_tracker_in_thread,
 tracker_thread2 = multiprocessing.Process(target=run_tracker_in_thread,
                                    args=(video_2, model, 2))
 
+tracker_thread3 = multiprocessing.Process(target=run_tracker_in_thread,
+                                   args=(video_3, model, 3))
+
+tracker_thread4 = multiprocessing.Process(target=run_tracker_in_thread,
+                                   args=(video_4, model, 4))
+
 
 
 tracker_thread1.start()
 tracker_thread2.start()
+tracker_thread3.start()
+tracker_thread4.start()
 cv2.destroyAllWindows()
